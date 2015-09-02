5ee39ca51a90c75cafa321b2f35dec86c22e6f00
matlink <matlink@matlink.fr>
2015-09-02 17:23:32 +0200

    [upd][0.1.3.2] fix some issues

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 from setuptools import setup, Command, find_packages
-import os, glob, stat
+from setuptools.command.install import install
+import os, glob, stat, sys
+
+class ChmodXCommand(install):
+    """Make /etc/init.d/gplayweb executable"""
+    def run(self):
+        install.run(self)
+        # Make service executable
+        st = os.stat('/etc/init.d/gplayweb')
+        os.chmod('/etc/init.d/gplayweb', st.st_mode | stat.S_IEXEC)
 
 templates = [f for f in glob.glob(os.path.join('templates','*'))]
 fonts = [f for f in glob.glob(os.path.join('static/fonts','*'))]
 css = [f for f in glob.glob(os.path.join('static/css','*'))]
 
 setup(name='GPlayWeb',
-        version='0.1.3',
+        version='0.1.3.2',
         description='GPlayWeb, A GPlayCli web interface',
         author="Matlink",
         author_email="matlink@matlink.fr",
         url="https://github.com/matlink/gplayweb",
         license="AGPLv3",
+        cmdclass={
+            'install': ChmodXCommand,
+        },
         scripts=['gplayweb'],
         data_files=[
             ['/etc/gplayweb/', ['gplayweb.conf.example']],
@@ -25,6 +37,4 @@ setup(name='GPlayWeb',
             'tornado',
         ],
     )
-# Make service executable
-st = os.stat('/etc/init.d/gplayweb')
-os.chmod('/etc/init.d/gplayweb', st.st_mode | stat.S_IEXEC)
+
